5b137c33e0154c89fa2a5eaa70fd67ab73d3e810
liyong2123 <48160169+liyong2123@users.noreply.github.com>
2020-01-14 18:33:32 -0500

    Updated

--- Scripts/ncviewer.py
+++ Scripts/ncviewer.py
@@ -1,30 +1,4 @@
-#import gdal
 import netCDF4
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import colors
-import plotly.graph_objects as go
-import os
-#import earthpy as et
-#import earthpy.spatial as es
-import earthpy.plot as ep
-
-
-xaxis =[]
-
-ds = ""
-for root, dirs, files in os.walk(os.getcwd()):
-    files.sort()
-    for f in files:
-        if f.endswith(".nc"):
-            ds = f
-
-fileOpen = netCDF4.Dataset(ds, "r")
-
-lats = fileOpen.dimensions['x']
-
-
-lons = fileOpen.dimensions['y']import netCDF4
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
@@ -32,105 +6,65 @@ import os
 import earthpy.plot as ep
 
 xaxis, ds = [], ""
+# Finds the .nc file in directory
 for root, dirs, files in os.walk(os.getcwd()):
     files.sort()
     for f in files:
         if f.endswith(".nc"):
             ds = f
 
+# Uses netcdf4 library to open the file
 fileOpen = netCDF4.Dataset(ds, "r")
-lats, lons, depth = fileOpen.dimensions['x'], fileOpen.dimensions['y'], fileOpen.dimensions['Bands']
+# Gets the dimensions of data
+lats, lons, depth2 = fileOpen.dimensions['x'], fileOpen.dimensions['y'], fileOpen.dimensions['Bands']
 
-print("Max x = "+str(lons.size - 1))
-print("Max y = "+str(lats.size - 1))
+# Debug
+print("Max x = " + str(lons.size - 1))
+print("Max y = " + str(lats.size - 1))
+print(depth2.size)
+#
 
-depth, arr1, xaxis, a = depth.size, [], list(range(1, depth)), 0
+depth, arr1, xaxis, a = depth2.size, [], list(range(1, depth2.size)), 0
 R, G, B = 29, 21, 16
 
-arrm = fileOpen.variables["Data"]
-arr = np.array([arrm[R, :, :], arrm[G, :, :], arrm[B, :, :]])
+arrmain = np.zeros(fileOpen.variables["Data"].shape)
+arrtemp = fileOpen.variables["Data"]
 
+# Apply transformation to the arrays so it's right orientation
+for c in range(0, depth):
+    arrmain[c] = np.fliplr(np.rot90(arrtemp[c, :, :], 2))
+
+# Create new plot and insert data
 fig, ax = plt.subplots(figsize=(6, 6))
-ep.plot_rgb(arr, rgb=(0, 1, 2), ax=ax, title="HyperSpectral Image")
+# Uses the RGB bands, 29, 21, 16
+ep.plot_rgb(arrmain, rgb=(29, 21, 16), ax=ax, title="HyperSpectral Image")
 
 
+# Function event listenter, once detectede right click, will generat nwe graph
 def onclick(event):
     if event.xdata is not None and event.ydata is not None and event.button == 3:
+        # (x,y) from click
         y = int(event.xdata)
         x = int(event.ydata)
 
         arr2 = []
         print(depth)
+        # Gets data from the click location (x,y) for all bands
         for b in range(1, depth):
-            data = arrm[b][x][y]
+            data = arrmain[b][x][y]
             arr2.append(data)
-        print("A")
-        # plt.close()
+
+        # Creates new graph
         fig2 = go.Figure()
-        fig2.add_trace(go.Scatter(x=xaxis, y=arr2, name="("+str(x)+","+str(y)+")",
-                       line=dict(color='firebrick', width=2)))
-        fig2.update_layout(title="Band Information for " + "("+str(x)+","+str(y)+")",
+        fig2.add_trace(go.Scatter(x=xaxis, y=arr2, name="(" + str(x) + "," + str(y) + ")",
+                                  line=dict(color='firebrick', width=2)))
+        fig2.update_layout(title="Band Information for " + "(" + str(x) + "," + str(y) + ")",
                            xaxis_title='Band Number',
                            yaxis_title='Value')
-
+        # displays graph onto web browser
         fig2.show()
 
 
+# Adds event listener for right clicks
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
 plt.show()
-
-
-
-depth = fileOpen.dimensions['Bands']
-
-
-print("Max x = "+str(lons.size - 1))
-print("Max y = "+str(lats.size - 1))
-
-arr1 = []
-
-depth = depth.size
-
-arr = []
-
-xaxis = list(range(1,depth))
-
-#for i in range(1,depth):
- #   xaxis.append(i)
-
-a = 0
-
-#arr_st,meta = es.stack(arr1,nodata=-9999)
-
-arrm = fileOpen.variables["Data"]
-
-arr = np.array([arrm[29, :, :], arrm[21, :, :],arrm[16, :, :]])
-
-fig, ax = plt.subplots(figsize=(6,6))
-
-ep.plot_rgb(arr, rgb=(0, 1, 2), ax=ax, title="HyperSpectral Image")
-
-def onclick(event):
-    if event.xdata != None and event.ydata != None and event.button == 3:
-        y = int(event.xdata)
-        x = int(event.ydata)
-
-        arrTemp = []
-        for a in range (1,depth + 1):
-            dataTemp = arrm[a]
-            arrTemp.append(dataTemp)
-        #plt.close()
-        a = 1
-        fig2 = go.Figure()
-        fig2.add_trace(go.Scatter(x=xaxis, y=arrTemp, name="("+str(x)+","+str(y)+")",
-                         line=dict(color='firebrick', width=2)))
-        fig2.update_layout(title="Band Information for "+ "("+str(x)+","+str(y)+")",
-                   xaxis_title='Band Number',
-                   yaxis_title='Value')
-
-        
-        fig2.show()
-
-cid = fig.canvas.mpl_connect('button_press_event', onclick)
-plt.show()
-
