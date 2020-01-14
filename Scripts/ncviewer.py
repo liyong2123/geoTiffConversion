5416f707c92bd7c1cf57b1a32f6bb09b05527978
Yong <liyong2123@gmail.com>
2020-01-14 15:52:27 -0500

    Fixed

--- Scripts/ncviewer.py
+++ Scripts/ncviewer.py
@@ -13,16 +13,17 @@ for root, dirs, files in os.walk(os.getcwd()):
             ds = f
 
 fileOpen = netCDF4.Dataset(ds, "r")
-lats, lons, depth = fileOpen.dimensions['x'], fileOpen.dimensions['y'], fileOpen.dimensions['Bands']
+lats, lons, depth2 = fileOpen.dimensions['x'], fileOpen.dimensions['y'], fileOpen.dimensions['Bands']
 
-print("Max x = " + str(lons.size - 1))
-print("Max y = " + str(lats.size - 1))
-
-depth, arr1, xaxis, a = depth.size, [], list(range(1, depth)), 0
+print("Max x = "+str(lons.size - 1))
+print("Max y = "+str(lats.size - 1))
+print(depth2.size)
+depth, arr1, xaxis, a = depth2.size, [], list(range(1, depth2.size)), 0
 R, G, B = 29, 21, 16
 
 arrm = fileOpen.variables["Data"]
-arr = np.array([arrm[R, :, :], arrm[G, :, :], arrm[B, :, :]])
+arr = np.array([np.fliplr(np.rot90(arrm[R, :, :], 2)), np.fliplr(np.rot90(arrm[G, :, :], 2)), np.fliplr(np.rot90(arrm
+                [B, :, :], 2))])
 
 fig, ax = plt.subplots(figsize=(6, 6))
 ep.plot_rgb(arr, rgb=(0, 1, 2), ax=ax, title="HyperSpectral Image")
@@ -41,62 +42,9 @@ def onclick(event):
         print("A")
         # plt.close()
         fig2 = go.Figure()
-        fig2.add_trace(go.Scatter(x=xaxis, y=arr2, name="(" + str(x) + "," + str(y) + ")",
-                                  line=dict(color='firebrick', width=2)))
-        fig2.update_layout(title="Band Information for " + "(" + str(x) + "," + str(y) + ")",
-                           xaxis_title='Band Number',
-                           yaxis_title='Value')
-
-        fig2.show()
-
-
-cid = fig.canvas.mpl_connect('button_press_event', onclick)
-plt.show()
-
-depth = fileOpen.dimensions['Bands']
-
-print("Max x = " + str(lons.size - 1))
-print("Max y = " + str(lats.size - 1))
-
-arr1 = []
-
-depth = depth.size
-
-arr = []
-
-xaxis = list(range(1, depth))
-
-# for i in range(1,depth):
-#   xaxis.append(i)
-
-a = 0
-
-# arr_st,meta = es.stack(arr1,nodata=-9999)
-
-arrm = fileOpen.variables["Data"]
-
-arr = np.array([arrm[29, :, :], arrm[21, :, :], arrm[16, :, :]])
-
-fig, ax = plt.subplots(figsize=(6, 6))
-
-ep.plot_rgb(arr, rgb=(0, 1, 2), ax=ax, title="HyperSpectral Image")
-
-
-def onclick(event):
-    if event.xdata != None and event.ydata != None and event.button == 3:
-        y = int(event.xdata)
-        x = int(event.ydata)
-
-        arrTemp = []
-        for a in range(1, depth + 1):
-            dataTemp = arrm[a]
-            arrTemp.append(dataTemp)
-        # plt.close()
-        a = 1
-        fig2 = go.Figure()
-        fig2.add_trace(go.Scatter(x=xaxis, y=arrTemp, name="(" + str(x) + "," + str(y) + ")",
-                                  line=dict(color='firebrick', width=2)))
-        fig2.update_layout(title="Band Information for " + "(" + str(x) + "," + str(y) + ")",
+        fig2.add_trace(go.Scatter(x=xaxis, y=arr2, name="("+str(x)+","+str(y)+")",
+                       line=dict(color='firebrick', width=2)))
+        fig2.update_layout(title="Band Information for " + "("+str(x)+","+str(y)+")",
                            xaxis_title='Band Number',
                            yaxis_title='Value')
 
